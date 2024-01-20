0424bfb11e7688f8bb7952feaa3bf00f1ed94fe5
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-01-20 16:36:53 -0600

    added functions to save button, began getItem for each timeblock

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var hour9 = $('#hour-9');
-var hour10 = $('#hour-10');
-var hour11 = $('#hour-11')
-var hour12 = $('#hour-12')
-var hour13 = $('#hour-1');
-var hour14 = $('#hour-2');
-var hour15 = $('#hour-3');
-var hour16 = $('#hour-4');
-var hour17 = $('#hour-5');
-
-    // $(".saveBtn").on("click", function () {
-        
-    //     console.log(this);
-    //     var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
-        // var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
-
-        //set items in local storage.
-    //     localStorage.setItem(time, text);
-    // })
-
+  
   
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -30,33 +11,32 @@ var hour17 = $('#hour-5');
     // useful when saving the description in local storage?
 
         $(".saveBtn").on("click", function () {
-        
-        console.log(this);
+          console.log(this)
         var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
         var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
-    });
+             //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+
+    $('#hour8 .description').val(localStorage.getItem('hour8'));
+    $('#hour9 .description').val(localStorage.getItem('hour9'));
+    $('#hour10 .description').val(localStorage.getItem('hour10'));
+    $('#hour11 .description').val(localStorage.getItem('hour11'));
+    $('#hour12 .description').val(localStorage.getItem('hour12'));
+    $('#hour13 .description').val(localStorage.getItem('hour13'));
+    $('#hour14 .description').val(localStorage.getItem('hour14'));
+    $('#hour15 .description').val(localStorage.getItem('hour15'));
+    $('#hour16 .description').val(localStorage.getItem('hour16'));
+    $('#hour17 .description').val(localStorage.getItem('hour17'));
+
+    
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    function(currentHour) {//or should this be the opposite of current hour?
-    var 
-    if (currentHour) {
+ 
 
-      //is false? but how to break into past present and future 
-      //array?
-  //make this show gray for past
-
-    } else {
-      //is true?
-  //make this show red for present
-    }else {
-      //make this show green for future
-    }
-
-    }
-    //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
@@ -66,8 +46,10 @@ var hour17 = $('#hour-5');
     var today = dayjs();
     $('#currentDay').text(today.format('MMM D, YYYY'));  
  });
-  //README
-//   GIVEN I am using a daily planner to create a schedule
+  
+
+
+// GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // DONE**THEN the current day is displayed at the top of the calendar
 // WHEN I scroll down
